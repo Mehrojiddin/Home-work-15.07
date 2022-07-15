517eaf9b5d7d682e7a61bb17d8b218858a8d9dd2
Mehrojiddin <mehrojiarkom@gmail.com>
2022-07-15 19:42:09 +0500

    Task 3 3. Add up the Numbers from a Single Number

--- js/main.js
+++ js/main.js
@@ -9,29 +9,49 @@
 
 //Task2
 
-let eror = prompt (`"Error Messages"
-1: "Check the fan: e1"
-2: "Аварийная остановка: e2"
-3: "Pump Error: e3"
-4: "c: e4"
-5: "Ошибка датчика температуры: e5"`)
+// let eror = prompt (`"Error Messages"
+// 1: "Check the fan: e1"
+// 2: "Аварийная остановка: e2"
+// 3: "Pump Error: e3"
+// 4: "c: e4"
+// 5: "Ошибка датчика температуры: e5"`)
 
-if(eror==1){
-    alert("Check the fan: e1")
-}
-if(eror==2){
-    alert("Аварийная остановка: e2")
-}
+// if(eror==1){
+//     alert("Check the fan: e1")
+// }
+// if(eror==2){
+//     alert("Аварийная остановка: e2")
+// }
 
-if(eror==3){
-    alert("Pump Error: e3")
-}
+// if(eror==3){
+//     alert("Pump Error: e3")
+// }
 
-if(eror==4){
-    alert("c: e4")
-}
+// if(eror==4){
+//     alert("c: e4")
+// }
 
-if(eror==5){
-    alert("Ошибка датчика температуры: e5")
-}
+// if(eror==5){
+//     alert("Ошибка датчика температуры: e5")
+// }
+
+
+
+//Task 3
+
+// let number = +prompt ("Enter your number")
+// let res=0
+
+// for(let i=0; i<=number; i++)
+// {
+// res=res+i
+// }
+// alert(res)
+
+
+
+//Task 4
+
+
+
 
